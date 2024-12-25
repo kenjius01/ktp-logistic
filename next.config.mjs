e965b0a7aea9d990851d50f7c0a20a65a5dc7946
kt <kenjius01@gmail.com>
2024-12-25 09:51:11 +0700

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -8,14 +8,6 @@ const nextConfig = {
         port: '',
         pathname: '**',
       },
-      {
-        protocol: 'https',
-        hostname: 'cdn.mhdigital.vn',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cms-api.thinhphat-logistics.com',
-      },
     ],
   },
 };
